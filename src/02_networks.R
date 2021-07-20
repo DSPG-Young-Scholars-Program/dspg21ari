df9b791bce3609bc6ecbff6844bf915d554a4212
Morgan Stockham <morganstockham@gmail.com>
2021-07-20 14:37:15 -0400

    changed network df

--- src/02_networks.R
+++ src/02_networks.R
@@ -13,3 +13,43 @@ install.packages(c("igraph","graphlayouts","ggraph","ggplot2"))
 # MOS - Skill - Salary or Employment (size)
 # MOS - Onetname - Employment (size)
 # Onetname - Skill - Employment (size) - MOS (color)
+
+# Data Frames to Export + Use
+socs <- read_rds("./data/working/soc_skill_bls_long.Rds")
+crosswalk <- read_excel("./data/original/crosswalk.xlsx")
+
+# MOS to SOC (Long)
+mos_skill_long <- left_join(crosswalk, socs, by = c("O*NET-SOC Code" = "onet"))
+soc_mos <- mos_skill_long %>% transmute(soure = `Army MOS Title`, target = socname, employ = tot_emp, salary = as.numeric(a_mean)) %>% unique()
+soc_mos <- na.omit(soc_mos)
+soc_mos <- soc_mos %>% group_by(target) %>% mutate(freq = n()) %>% ungroup()
+soc_mos <- soc_mos %>%
+  mutate(e_total=sum(employ)) %>%
+  group_by(target) %>%
+  mutate(e_weight=sum(employ/e_total)) %>%
+  ungroup()
+soc_mos <- soc_mos %>%
+  mutate(s_total=sum(salary)) %>%
+  group_by(target) %>%
+  mutate(s_weight=sum(salary/s_total)) %>%
+  ungroup()
+write.csv(soc_mos, "./data/working/mos_soc_network.csv")
+
+# MOS to Skill (Long)
+skill_mos <- mos_skill_long %>% transmute(source = `Army MOS Title`, target = skill, employ = tot_emp, salary = as.numeric(a_mean)) %>% unique()
+skill_mos <- na.omit(skill_mos)
+skill_mos <- skill_mos %>% group_by(target) %>% mutate(freq = n()) %>% ungroup()
+skill_mos <- skill_mos %>%
+  mutate(e_total=sum(employ)) %>%
+  group_by(target) %>%
+  mutate(e_weight=sum(employ/e_total)) %>%
+  ungroup()
+skill_mos <- skill_mos %>%
+  mutate(s_total=sum(salary, na.omit = T)) %>%
+  group_by(target) %>%
+  mutate(s_weight=sum(salary/s_total)) %>%
+  ungroup()
+write.csv(skill_mos, "./data/working/mos_skill_network.csv")
+
+# MOS to MOS (Adjacency Matrix)
+
--- src/03_soc_mos.R
+++ src/03_soc_mos.R
@@ -10,16 +10,15 @@ mos_skills<-readRDS("data/working/mos_skill.Rds") %>% as.data.table()
 soc_skills<-readRDS("data/working/soc_skill_bls_long.Rds")%>% as.data.table()
 soc<-readRDS("data/working/soc_skill_bls.Rds")
 
-#pull top 40 jobs by employment + annual median income
+#pull top 40 jobs by employment + annual median income - commmented
 soc<-soc[order(-tot_emp, -a_median)]
-soc_top_20<-soc[1:20, ]$onetname
+# soc_top_20<-soc[1:20, ]$onetname
 
 ### 2:
-#order soc skills by employment + median income
+#order soc skills by employment + median income - morgan removed selecting top 30,000 skills
 soc_skills<-soc_skills[order(-tot_emp, -a_median)]
-soc_skills_top<-soc_skills[1:30000, ]
-skills_top<-as.data.table(table(soc_skills_top$skill))
-skills_top<-skills_top%>% arrange.(skills_top, desc(N))
+skills_top<-as.data.table(table(soc_skills$skill))
+skills_top<-skills_top%>% arrange(skills_top, desc(N))
 
 #match by most common skills
 dt_skill_temp<-data.table(mos=mos_skills[1:11,1], skill=character())
@@ -41,6 +40,7 @@ setnames(dt_skill_temp, "mos.Army MOS Code", "Army MOS Code")
 mos_skills<-merge( mos_skills, dt_skill_temp,by="Army MOS Code")
 #mos_skills<-as.data.frame(mos_skills)
 write_rds(mos_skills, "./data/working/mos_soc.Rds")
+write.csv(mos_skills, "./data/working/mos_soc.csv", col.names = T, row.names = F)
 
 #quick personal sanity check
 sanity_check<-matrix(nrow=10, ncol=10)
