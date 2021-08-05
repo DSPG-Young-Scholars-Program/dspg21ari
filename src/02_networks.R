fbc92839d96e95f2380dcb48fa837535358dd1f7
Morgan Stockham <morganstockham@gmail.com>
2021-08-04 22:24:54 -0400

    last update before thrusday:

--- src/02_networks.R
+++ src/02_networks.R
@@ -1,11 +1,11 @@
 library(tidyverse)
 library(readxl)
-library(igraph)
-library(ggraph)
-library(tidygraph)
-library(graphlayouts)
-library(visNetwork)
-library(networkD3)
+# library(igraph)
+# library(ggraph)
+# library(tidygraph)
+# library(graphlayouts)
+# library(visNetwork)
+# library(networkD3)
 
 #
 # Network Visualizations -----------------------------
@@ -18,7 +18,6 @@ library(networkD3)
 # Visualizations
 # MOS - Skill - Salary or Employment (size)
 # MOS - Onetname - Employment (size)
-setwd("/home/mes5bu/git/ari3")
 # Data Frames to Export + Use
 socs <- read_rds("./data/working/soc_skill_bls_long.Rds")
 crosswalk <- read_excel("./data/original/crosswalk.xlsx")
@@ -42,9 +41,16 @@ soc_mos <- soc_mos %>%
   mutate(s_freq = s_weight*freq)
 
 # MOS to Skill (Long)
-skill_mos <- mos_skill_long %>% transmute(source = `Army MOS Title`, target = skill, employ = tot_emp, salary = as.numeric(a_mean)) %>% unique()
+skill_mos <- mos_skill_long %>% transmute(source = `Army MOS Title`, target = skill, employ = tot_emp, salary = as.numeric(a_mean)) %>%
+  group_by(source, target) %>%
+  mutate(n = n()) %>%
+  ungroup %>%
+  unique()
 skill_mos <- na.omit(skill_mos)
-skill_mos <- skill_mos %>% group_by(target) %>% mutate(freq = n(), employ = mean(employ), salary = mean(salary)) %>% ungroup() %>% unique()
+skill_mos <- skill_mos %>% group_by(source, target) %>% mutate(freq2 = n()) %>% ungroup() %>%group_by(target) %>%
+  mutate(freq = n(), employ = mean(employ), salary = mean(salary), sd1=sd(n), sd2 = sd(freq), sd3=sd(freq2)) %>%
+  ungroup() %>%
+  unique()
 skill_mos <- skill_mos %>%
   mutate(e_total=sum(employ)) %>%
   group_by(target) %>%
@@ -57,6 +63,9 @@ skill_mos <- skill_mos %>%
   mutate(s_weight=sum(salary/s_total)) %>%
   ungroup() %>%
   mutate(s_freq = s_weight*freq)
+
+write.csv(soc_mos, "./data/working/soc_network.csv", row.names = F)
+write.csv(skill_mos, "./data/working/skill_network.csv", row.names = F)
 
 # Reduce DF -------------------------
 mos_unique <- soc_mos %>%
@@ -100,9 +109,15 @@ all_long <- read_rds("/sfs/qumulo/qhome/mes5bu/git/ari3/data/working/all_soc_ski
 mos_skill_long <- left_join(crosswalk, all_long, by = c("O*NET-SOC Code" = "onet"))
 mos_skill_long$skill_type <- ifelse(mos_skill_long$isbaseline == T, "Baseline",
                                     ifelse(mos_skill_long$issoftware == T, "Software", "Specialized"))
-skill_mos <- mos_skill_long %>% transmute(source = `Army MOS Title`, target = skill, employ = tot_emp, salary = as.numeric(a_mean), skill_type) %>% unique()
+skill_mos <- mos_skill_long %>% transmute(source = `Army MOS Title`, target = skill, employ = tot_emp, salary = as.numeric(a_mean)) %>%
+  group_by(source, target) %>%
+  mutate(n = n()) %>%
+  ungroup %>%
+  unique()
 skill_mos <- na.omit(skill_mos)
-skill_mos <- skill_mos %>% group_by(target) %>% mutate(freq = n(), employ = mean(employ), salary = mean(salary)) %>% ungroup() %>% unique()
+skill_mos <- skill_mos %>% group_by(source, target) %>% mutate(freq2 = n()) %>% ungroup() %>% group_by(target) %>%
+  mutate(freq = n(), employ = mean(employ), salary = mean(salary), sd1=sd(n), sd2 = sd(freq)) %>%
+  ungroup() %>% unique()
 skill_mos <- skill_mos %>%
   mutate(e_total=sum(employ)) %>%
   group_by(target) %>%
@@ -140,43 +155,43 @@ write.csv(skill_mos_unique, "./data/working/all_skill_unique.csv", row.names = F
 # https://www.jessesadler.com/post/network-analysis-with-r/
 
 # iGraph
-mos <- skill_mos %>%
-  distinct(source) %>%
-  rename(label = source)
-skills <- skill_mos %>%
-  distinct(target) %>%
-  rename(label = target)
-nodes <- full_join(mos, skills, by = "label")
-
-skill_network <- graph_from_data_frame(d = skill_mos, vertices = nodes, directed = TRUE)
-plot(skill_network, edge.arrow.size = 0.2)
-plot(skill_network, layout = layout_with_graphopt, edge.arrow.size = 0.2)
-
-# Tidygraph
-skill_tidy <- as_tbl_graph(skill_network)
-
-ggraph(skill_tidy, layout = "graphopt") +
-  geom_node_point() +
-  geom_edge_link(aes(width = e_freq), alpha = 0.8) +
-  scale_edge_width(range = c(0.2, 2)) +
-  geom_node_text(aes(label = name), repel = TRUE) +
-  labs(edge_width = "Employment\n Weighted\n Frequency") +
-  theme_graph()
-
-# VisNetwork
-nodes2 <- nodes %>% rename(id = label)
-edges2 <- skill_mos %>% transmute(from = source, to = target, weight = e_freq)
-
-visNetwork(nodes2, edges2)
-visIgraph(skill_network)
-visNetwork(nodes2, edges2) %>%
-  visIgraphLayout(layout = "layout_with_fr") %>%
-  visEdges(arrows = "middle")
-
-# D3
-nodesd3 <- as.data.frame(nodes2)
-edgesd3 <- as.data.frame(edges2)
-
-forceNetwork(Links = edgesd3, Nodes = nodesd3, Source = "from", Target = "to",
-             NodeID = "id", Group = "id", Value = "weight",
-             opacity = 1, fontSize = 16, zoom = TRUE)
+# mos <- skill_mos %>%
+#   distinct(source) %>%
+#   rename(label = source)
+# skills <- skill_mos %>%
+#   distinct(target) %>%
+#   rename(label = target)
+# nodes <- full_join(mos, skills, by = "label")
+#
+# skill_network <- graph_from_data_frame(d = skill_mos, vertices = nodes, directed = TRUE)
+# plot(skill_network, edge.arrow.size = 0.2)
+# plot(skill_network, layout = layout_with_graphopt, edge.arrow.size = 0.2)
+#
+# # Tidygraph
+# skill_tidy <- as_tbl_graph(skill_network)
+#
+# ggraph(skill_tidy, layout = "graphopt") +
+#   geom_node_point() +
+#   geom_edge_link(aes(width = e_freq), alpha = 0.8) +
+#   scale_edge_width(range = c(0.2, 2)) +
+#   geom_node_text(aes(label = name), repel = TRUE) +
+#   labs(edge_width = "Employment\n Weighted\n Frequency") +
+#   theme_graph()
+#
+# # VisNetwork
+# nodes2 <- nodes %>% rename(id = label)
+# edges2 <- skill_mos %>% transmute(from = source, to = target, weight = e_freq)
+#
+# visNetwork(nodes2, edges2)
+# visIgraph(skill_network)
+# visNetwork(nodes2, edges2) %>%
+#   visIgraphLayout(layout = "layout_with_fr") %>%
+#   visEdges(arrows = "middle")
+#
+# # D3
+# nodesd3 <- as.data.frame(nodes2)
+# edgesd3 <- as.data.frame(edges2)
+#
+# forceNetwork(Links = edgesd3, Nodes = nodesd3, Source = "from", Target = "to",
+#              NodeID = "id", Group = "id", Value = "weight",
+#              opacity = 1, fontSize = 16, zoom = TRUE)
